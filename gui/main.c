59eb0eaa519fb656405376268512fbcdd9903790
Martin.Yang <yjjfirst@hotmail.com>
2016-08-18 14:32:43 +0800

    Very simple GUI layout.

--- gui/main.c
+++ gui/main.c
@@ -1,12 +1,80 @@
 #include <gtk/gtk.h>
 
+static void
+print_hello (GtkWidget *widget,
+             gpointer   data)
+{
+  g_print ("Hello World\n");
+}
+
+static void add_all_widget(GtkWidget *window)
+{
+    GtkWidget *make_call_button;
+    GtkWidget *number;
+    GtkWidget *status_label;
+    GtkWidget *digits_grid_box;
+    GtkWidget *vbox;
+    GtkWidget *button;
+
+    
+    make_call_button = gtk_button_new_with_label ("Make Call");
+    g_signal_connect_swapped (make_call_button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+    
+    number = gtk_entry_new();
+    vbox =  gtk_box_new(GTK_ORIENTATION_VERTICAL, 1);
+
+    status_label = gtk_label_new("Status:");
+    gtk_label_set_xalign((GtkLabel *)status_label, 0);
+
+    digits_grid_box = gtk_grid_new();
+    
+    button = gtk_button_new_with_label ("1");
+    gtk_grid_attach (GTK_GRID (digits_grid_box), button, 0, 0, 1, 1);
+    button = gtk_button_new_with_label ("2");
+    gtk_grid_attach (GTK_GRID (digits_grid_box), button, 1, 0, 1, 1);    
+    button = gtk_button_new_with_label ("3");
+    gtk_grid_attach (GTK_GRID (digits_grid_box), button, 2, 0, 1, 1);    
+
+    button = gtk_button_new_with_label ("4");
+    gtk_grid_attach (GTK_GRID (digits_grid_box), button, 0, 1, 1, 1);
+    button = gtk_button_new_with_label ("5");
+    gtk_grid_attach (GTK_GRID (digits_grid_box), button, 1, 1, 1, 1);    
+    button = gtk_button_new_with_label ("6");
+    gtk_grid_attach (GTK_GRID (digits_grid_box), button, 2, 1, 1, 1);    
+
+    button = gtk_button_new_with_label ("7");
+    gtk_grid_attach (GTK_GRID (digits_grid_box), button, 0, 2, 1, 1);
+    button = gtk_button_new_with_label ("8");
+    gtk_grid_attach (GTK_GRID (digits_grid_box), button, 1, 2, 1, 1);    
+    button = gtk_button_new_with_label ("9");
+    gtk_grid_attach (GTK_GRID (digits_grid_box), button, 2, 2, 1, 1);    
+
+    button = gtk_button_new_with_label ("*");
+    gtk_grid_attach (GTK_GRID (digits_grid_box), button, 0, 3, 1, 1);
+    button = gtk_button_new_with_label ("0");
+    gtk_grid_attach (GTK_GRID (digits_grid_box), button, 1, 3, 1, 1);    
+    button = gtk_button_new_with_label ("#");
+    gtk_grid_attach (GTK_GRID (digits_grid_box), button, 2, 3, 1, 1);    
+
+    
+    gtk_box_pack_start(GTK_BOX (vbox), number, FALSE, FALSE, 1);
+    gtk_box_pack_start(GTK_BOX (vbox), status_label, FALSE, FALSE, 1);
+    gtk_box_pack_start(GTK_BOX (vbox), digits_grid_box, TRUE, TRUE, 1);
+    gtk_box_pack_end(GTK_BOX(vbox), make_call_button, FALSE, FALSE, 1);
+
+    gtk_container_add(GTK_CONTAINER(window), vbox);    
+}
+
 static void activate (GtkApplication* app, gpointer user_data)
 {
     GtkWidget *window;
-
+    
     window = gtk_application_window_new (app);
-    gtk_window_set_title (GTK_WINDOW (window), "Window");
-    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+    gtk_window_set_title (GTK_WINDOW (window), "X-SIP");
+    gtk_window_set_default_size (GTK_WINDOW (window), 300, 200);
+
+    add_all_widget(window);
+    
     gtk_widget_show_all (window);
 }
 
